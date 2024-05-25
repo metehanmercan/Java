6ddd4dbd214773103faea19e7588c9f450ec423b
metehanmercan <mthnmrcn0133@gmail.com>
2024-05-25 20:50:24 +0300

    main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,17 +6,6 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        String iconPath = "/png.png";
-
-        // ImageIcon oluşturma
-        ImageIcon customIcon = new ImageIcon(Main.class.getResource(iconPath));
-
-
-
-        // UIManager kullanarak bilgi mesajı simgesini değiştirme
-        UIManager.put("OptionPane.informationIcon", customIcon);
-
-        // Örnek bir bilgi mesajı gösterme
-        JOptionPane.showMessageDialog(null, "Bu bir bilgi mesajıdır.", "Bilgi", JOptionPane.INFORMATION_MESSAGE);
+        System.out.println("hello");
     }
-    }
+}
