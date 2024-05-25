1ab85e4d06d38c955062106f6b1ba8284dfca961
metehanmercan <mthnmrcn0133@gmail.com>
2024-05-25 20:58:20 +0300

    yeni icon

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,17 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        String iconPath = "/png.png";
+
+        // ImageIcon oluşturma
+        ImageIcon customIcon = new ImageIcon(Main.class.getResource(iconPath));
+
+
+
+        // UIManager kullanarak bilgi mesajı simgesini değiştirme
+        UIManager.put("OptionPane.informationIcon", customIcon);
+
+        // Örnek bir bilgi mesajı gösterme
+        JOptionPane.showMessageDialog(null, "Bu bir bilgi mesajıdır.", "Bilgi", JOptionPane.INFORMATION_MESSAGE);
     }
-}
+    }
